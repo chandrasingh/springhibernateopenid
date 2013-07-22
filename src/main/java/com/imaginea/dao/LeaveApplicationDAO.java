38515373d141332c69098707e9fed44b08faab8e
chandrasingh <chandra.singh@imaginea.com>
2013-07-22 15:36:36 +0530

    deleting old dao n services

--- src/main/java/com/imaginea/dao/LeaveApplicationDAO.java
+++
@@ -1,9 +0,0 @@
-package com.imaginea.dao;
-
-import com.imaginea.model.LeaveApplication;
-
-public interface LeaveApplicationDAO {
-	public void addLeave(LeaveApplication leave);
-
-	public void setLeaveStatus(Integer id, String status);
-}
--- src/main/java/com/imaginea/services/LeaveApplicationService.java
+++
@@ -1,9 +0,0 @@
-package com.imaginea.services;
-
-import com.imaginea.model.LeaveApplication;
-
-public interface LeaveApplicationService {
-	public void addLeave(LeaveApplication leave);
-
-	public void setLeaveStatus(Integer id, String status);
-}
